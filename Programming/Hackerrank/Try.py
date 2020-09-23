57b0495f532908382fd8d1dfbcb575f44a96d2e0
Dharmesh Harshwal <dharmesh.harshwal@gmail.com>
2020-09-24 00:19:13 +0530

    24-09-2020

--- Programming/Hackerrank/Try.py
+++ Programming/Hackerrank/Try.py
@@ -1,1 +1,13 @@
-print(0%2)
+def binary_search(arr, low, high, x):  
+    while high >= low: 
+        mid = low+((high-low)//2)
+        if (arr[mid]==x):
+            return mid
+        if (arr[mid]>x):
+            high=mid-1
+        if (arr[mid]<x):
+            low=mid+1
+    return mid+1   
+a=[1,2,3,4,5,6,7,8,10]
+z=binary_search(a,0,len(a)-1,9)
+print(z)
--- Programming/Hackerrank/leaderboard.py
+++ Programming/Hackerrank/leaderboard.py
@@ -1,14 +1,16 @@
+#a=[100,50,40,20,10]
+#5
+#a,0,4,5
 def binary_search(arr, low, high, x):  
-    if high >= low: 
-        mid = (high + low) // 2 
-        if arr[mid] == x: 
-            return mid 
-        elif arr[mid] > x: 
-            return binary_search(arr, low, mid - 1, x) 
-        else: 
-            return binary_search(arr, mid + 1, high, x) 
-    else: 
-        return mid
+    while high >= low: 
+        mid = low+((high-low)//2)
+        if (arr[mid]==x):
+            return mid+1
+        if (arr[mid]>x):
+            low=mid+1
+        if (arr[mid]<x):
+            high=mid-1
+    return mid+1 
 
 def uni(list1): 
     list_set = set(list1) 
@@ -21,6 +23,13 @@ a=uni(a)
 a.sort(reverse=True)
 x=int(input())
 b = list(map(int,input().strip().split()))[:x]
-index=0
 for i in range(0,x):
+    z=binary_search(a,0,len(a)-1,b[i])
+    if (b[i]>a[0]):
+        print(1)
+    elif (b[i]<a[len(a)-1]):
+        print(len(a)+1)
+    else:
+        print(z)
+
     
